caf7ac62316078a8829544700c7bbfd405b501f2
Harshdeep Sokhey <harshdeep.sokhey@gmail.com>
2016-04-24 21:35:12 +0530

    Added new file MergeSort.py for mergeSort modoule (initial Commit)

---
+++ sortingAndSearching/MergeSort.py
@@ -0,0 +1,31 @@
+## filename: MergeSort.py
+
+## sortingAndSearching
+## CS6101: Design and Analysis of Algorithms
+
+## Reference : 
+## http://www.geeksforgeeks.org
+## http://topcoder.com 
+## Introduction to Algorithms, CLRS
+
+## Description :
+## TODO
+
+####################################################
+
+from LinkedList import LinkedList
+
+class MergeSort:
+    def __init__(self, dataList):
+        self.unsortedList = dataList
+        self.sortedList = None
+
+    def sort(self):
+        pass
+
+    def merge(self):
+        pass
+
+    def display(self):
+        pass
+
