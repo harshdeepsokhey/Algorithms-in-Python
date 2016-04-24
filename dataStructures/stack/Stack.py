fb549b14c2f3a7e289f664ffafc429b300d2dae0
Harshdeep Sokhey <harshdeep.sokhey@gmail.com>
2016-04-25 04:12:50 +0530

    Implemented push, pop, isEmpty, isFull methods for Stack TODO: To implement getPostfix, getPrefix

--- dataStructures/stack/Stack.py
+++ dataStructures/stack/Stack.py
@@ -26,12 +26,16 @@
 ## SC : Space Complexity
 ## LIFO : Last In First Out
 ####################################################
+from LinkedList import LinkedList,Node
 
 class Stack:
     def __init__(self):
         self.top = None
+        self.stackList = None
+        self.stackMAX = 10
+        self.numElements = 0
 
-    def createStack(self, dataList):
+    def createStack(self, dataList, stackMax):
         '''
             createStack(dataList)
             Creates a stack with elements 
@@ -39,16 +43,41 @@ class Stack:
             TC :
             SC :
         '''
-        pass
+        n = len(dataList)
+        tempList = LinkedList()
+        for i in xrange(n):
+            if i >= stackMax:
+                print "Stack MAX Limit reached!"
+                break
+            tempList.push(dataList[i])
+            self.numElements += 1
 
-    def push(self):
+        self.stackList = tempList
+        self.top = tempList.head
+
+    def push(self, data):
         '''
             push()
             Pushes elements on to the stack (LIFO)
             TC :
             SC :
         '''
-        pass
+        if (self.top is None) and (self.stackList is None) :
+            print "No Stack Exist!"
+            return
+
+        if self.isFull():
+            print "Stack Overflow: Cannot add (",data,"), Stack is FULL! "
+            return
+
+        node = Node(data)
+        node.next = self.stackList.head
+        self.stackList.head = node
+        node.prev = None
+        self.top = node
+        self.numElements += 1
+        print "Adding ", self.top.data
+
 
     def pop(self):
         '''
@@ -57,16 +86,33 @@ class Stack:
             TC :
             SC :
         '''
-        pass
+        if (self.top is None) and (self.stackList is None) :
+            print "No Stack Exist!"
+            return
+
+        if self.isEmpty():
+            print "Stack Underflow: Stack is EMPTY!"
+            return
+
+        temp = self.top
+        print "Deleting ",temp.data
+        self.stackList.head = temp.next
+        self.top = temp.next
+        temp.next = None
+        self.numElements -= 1
 
     def getTop(self):
         return self.top
 
     def isEmpty(self):
-        pass
+        if self.numElements is 0:
+            return True
+        return False
 
     def isFull(self):
-        pass
+        if self.numElements >= self.stackMAX:
+            return True
+        return False
 
     def getPostFix(self):
         pass
@@ -75,12 +121,30 @@ class Stack:
         pass
 
     def display(self):
-        pass
+        temp = self.stackList.head
+
+        while(temp):
+            print temp.data,"->",
+            temp = temp.next
+        print "Null"
 
 
 if __name__ == '__main__':
     arr = [15,6,2,9,17,3]
     o_stack = Stack()
-    o_stack.createStack(arr)
+    o_stack.createStack(arr,10)
+    o_stack.display()
+    o_stack.push(10)
+    o_stack.push(20)
+    o_stack.push(30)
+    o_stack.push(40)
+    o_stack.push(50)
+    o_stack.display()
+    for i in xrange(10):
+        o_stack.pop()
+        o_stack.display()
+    o_stack.pop()
+    o_stack.push(60)
+    o_stack.display()
 
 
