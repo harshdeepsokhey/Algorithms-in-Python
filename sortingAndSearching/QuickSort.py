a3bc11ac2d56c0a788a3f3d6f34fb29c838d10f1
Harshdeep Sokhey <harshdeep.sokhey@gmail.com>
2016-04-26 01:59:22 +0530

    Added a new file for QuickSort implementation (Inital Commit)

---
+++ sortingAndSearching/QuickSort.py
@@ -0,0 +1,33 @@
+## filename: QuickSort.py
+
+## sortingAndSearching
+## CS6101: Design and Analysis of Algorithms
+
+## Reference : 
+## http://www.geeksforgeeks.org
+## http://topcoder.com 
+## Introduction to Algorithms, CLRS
+
+## Description :
+## TODO
+
+####################################################
+
+class QuickSort:
+    def __init__(self, arr):
+        self.unsortedList = arr
+
+    def sort(self):
+        pass
+    
+    def randomize(self):
+        pass
+
+    def partition(self):
+        pass
+
+    def display(self):
+        pass
+
+if __name__='__main__':
+    pass
