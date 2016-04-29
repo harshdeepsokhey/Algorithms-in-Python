a471ca591db1b2feff8fcba45f497b42cb8513d2
Harshdeep Sokhey <harshdeep.sokhey@gmail.com>
2016-04-30 02:26:56 +0530

    Change of file structure for BST

---
+++ dataStructures/binarySearchTree/BinarySearchTree.py
@@ -0,0 +1,56 @@
+## filename : BinaryTree.py 
+
+## data structures ##
+## CS6101: Design and Analysis of Algorithms
+
+## Reference : 
+## http://www.geeksforgeeks.org
+## http://topcoder.com 
+
+## Operations:
+## TODO
+####################################################
+
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.key = key
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+    
+    def createBST(self,sortedList):
+        pass
+    def insertChild(self, parent, child):
+    	pass
+
+    def getRootNode(self):
+    	return self.root
+
+    def deleteChild(self):
+        pass
+
+    def traversal_DFT(self):
+        pass
+
+    def traversal_BFT(self):
+        pass
+
+    def display(self):
+        pass
+
+    def inOrder(self, node):
+        pass
+
+    def postOrder(self,node):
+		pass
+
+    def preOrder(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
+
