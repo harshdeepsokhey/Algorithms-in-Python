861abdf6916a5e130f70c274f9f8be8c424a64a7
Harshdeep Sokhey <harshdeep.sokhey@gmail.com>
2016-04-30 02:27:38 +0530

    Added implementation for MergeSort (Inital Commit)

--- sortingAndSearching/MergeSort.py
+++ sortingAndSearching/MergeSort.py
@@ -6,26 +6,64 @@
 ## Reference : 
 ## http://www.geeksforgeeks.org
 ## http://topcoder.com 
-## Introduction to Algorithms, CLRS
+## Introduction to Algorithms - CLRS
+## Fundamentals of Computer Algorithms-Horowitz, Sahani, Rajasekaran
 
 ## Description :
 ## TODO
 
 ####################################################
-
-from LinkedList import LinkedList
+import math
 
 class MergeSort:
-    def __init__(self, dataList):
-        self.unsortedList = dataList
-        self.sortedList = None
+    def __init__(self,data):
+        self.dataList = data
+        self.auxList = [0]*len(data)
 
-    def sort(self):
-        pass
+    def sort(self,low,high):
+        if ( low < high):
+            mid = int(math.floor((low + high) /2))
+            self.sort(low,mid)
+            self.sort(mid+1, high)
+            self.merge(low,mid,high)
 
-    def merge(self):
-        pass
+    def merge(self, low, mid, high):
+        h = low
+        i = low
+        j = mid+1
+        
+        while ((h <= mid) and (j <= high)):
+            if self.dataList[h] <= self.dataList[j]:
+                self.auxList[i] = self.dataList[h]
+                h = h+1
+            else:
+                self.auxList[i] = self.dataList[j]
+                j = j+1
+            i = i+1
+
+        if h > mid:
+            k =0
+            for k in xrange(j,high+1):
+                self.auxList[i] = self.dataList[k]
+                i = i+1
+        else:
+            k=0
+            for k in xrange(h,mid+1):
+                self.auxList[i] = self.dataList[k]
+                i = i+1
+        k = 0
+        for k in xrange(low, high+1):
+            self.dataList[k] = self.auxList[k]
 
     def display(self):
-        pass
+        print "SortedList:"
+        print self.dataList
 
+
+if __name__ == '__main__':
+    arr = [310,285,179,652,351,423,861,254,450,520]
+    msort = MergeSort(arr)
+    print "UnSortedList:"
+    print arr
+    msort.sort(0,len(arr)-1)
+    msort.display()
