6b33c097431f7f7a21ae766d0c9a2e597075efa1
Harshdeep Sokhey <harshdeep.sokhey@gmail.com>
2016-06-01 22:59:34 +0530

    Added dummy for Shell Sort

---
+++ sortingAndSearching/ShellSort.py
@@ -0,0 +1,26 @@
+## filename: ShellSort.py
+
+## sortingAndSearching
+## CS6101: Design and Analysis of Algorithms
+
+## Reference : 
+## http://www.geeksforgeeks.org
+## http://topcoder.com 
+## Introduction to Algorithms, CLRS
+
+## Description :
+## TODO
+##
+## TC :
+## SC :
+####################################################
+
+class ShellSort:
+    def __init__(self,arr):
+        self.dataList = arr
+
+    def sort(self):
+        pass
+
+if __name__=='__main__':
+    pass
